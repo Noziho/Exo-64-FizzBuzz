fcc7ffbe5d80ce3d3a579ae3176be7580e595683
Noah2211 <noah.decroix3@gmail.com>
2021-11-15 22:06:30 +0100

    exercice done

---
+++ app.js
@@ -0,0 +1,26 @@
+let a = 1;
+
+
+
+while (a <= 100) {
+
+    if ((a % 3 === 0) && (a % 5 === 0)) {
+        console.log(a + "=> fizzbuzz");
+    }
+
+    else if (a % 3 === 0 ) {
+        console.log(a + "=> fizz");
+    }
+
+    else if (a % 5 === 0) {
+        console.log(a + "=> buzz");
+    }
+
+    else {
+        console.log(a);
+    }
+
+    a++;
+
+
+}
