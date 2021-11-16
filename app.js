e873739c352c75be95aba17be99ccd2f3052f550
Noah2211 <noah.decroix3@gmail.com>
2021-11-16 09:16:21 +0100

    minor changes

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ while (a <= 100) {
         console.log(a + "=> fizzbuzz");
     }
 
-    else if (a % 3 === 0 ) {
+    else if ((a % 3 === 0) && (a % 5 !== 0)) {
         console.log(a + "=> fizz");
     }
 
-    else if (a % 5 === 0) {
+    else if ((a % 5 === 0) && (a % 3 !== 0)) {
         console.log(a + "=> buzz");
     }
 
